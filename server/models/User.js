368bacc7d4762582081b46272a1d7e10c7145bda
Sai Sri Anjali N <322103310200@gvpce.ac.in>
2025-07-16 20:44:11 +0530

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,14 @@
+// server/models/User.js
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userId: { type: String, unique: true, required: true },
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema, 'user');
